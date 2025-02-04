433f227978f20508cc97da87825ddd2057df7348
Dhruv Krishna Vaid <dhruvkrishnavaid@gmail.com>
2025-02-04 21:42:20 +0530

    fix: update Flask app run configuration to use environment variable for debug mode

--- server.py
+++ server.py
@@ -275,4 +275,4 @@ def start():
 
 if __name__ == "__main__":
     debug_mode = os.getenv("FLASK_DEBUG", "False").lower() in ["true", "1", "t"]
-    app.run(debug=True)
+    app.run(debug=debug_mode)
